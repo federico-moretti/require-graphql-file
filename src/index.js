67f3523a936c97bb42cb88092db14fe59d690d40
Federico Moretti <dj.ikos90@gmail.com>
2018-01-31 00:08:01 +0100

    fix wrong file path

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+const caller = require('caller');
 
 function requireGraphQLFile(filename) {
+  const folder = path.dirname(caller());
   let file = filename.replace(/(.graphql$)/gi, '');
   file = `${file}.graphql`;
-  return fs.readFileSync(path.join(__dirname, file), 'utf8').toString();
+  return fs.readFileSync(path.join(folder, file), 'utf8').toString();
 }
 
 module.exports = requireGraphQLFile;
